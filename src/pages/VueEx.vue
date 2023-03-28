4bb3b86e6b3ba50b9ab573641edd5625da090ea0
ss1064wp <ss1064wp@gmail.com>
2023-03-28 11:25:06 +0900

    3

---
+++ src/pages/VueEx.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: '',
+    components:{},
+    data(){
+      return{
+
+      }
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
