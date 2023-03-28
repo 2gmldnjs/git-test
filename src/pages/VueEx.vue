3993796849f9c7c140064b394cce73b8ea2f1154
ss1064wp <ss1064wp@gmail.com>
2023-03-28 12:03:26 +0900

    3

--- src/pages/VueEx.vue
+++ src/pages/VueEx.vue
@@ -20,6 +20,42 @@
     </q-card-section>
     <q-separator />
 
+    <q-btn :disable="isButtonDisabled" label="버튼 보이기"></q-btn>
+    <q-separator />
+
+    <q-card-section>
+      {{ number +1 }}
+    </q-card-section>
+    <q-separator />
+    <q-card-section>{{ ok ? 'yes' : 'no' }}</q-card-section>
+    <q-separator />
+
+    <q-card-section>
+      {{ message.split("").reverse().join("") }}
+    </q-card-section>
+    <q-separator />
+
+    <q-card-section :id="`list-${id}`"> list-{{id}}</q-card-section>
+    <q-separator />
+
+    <!--<q-card-section class="text-h6">
+      {{ calculateDate() }}
+    </q-card-section>-->
+
+    <q-separator />
+    <q-card-section v-if="!seen"> Now you see me </q-card-section>
+    <q-card-section v-else> on no! </q-card-section>
+    <q-separator />
+    <q-btn @click="awesome = !awesome" label="toggle"></q-btn>
+    <q-card-section v-if="awesome"> Vue is awesome! </q-card-section>
+    <q-card-section v-else> on no! </q-card-section>
+    <q-separator />
+    <q-card-section v-if="type == 'A'"> A </q-card-section>
+    <q-card-section v-else-if="type === 'B'"> B </q-card-section>
+    <q-card-section v-else-if="type === 'C'"> C </q-card-section>
+    <q-card-section v-else> Not A/B/C </q-card-section>
+    <q-separator />
+
 </template>
 
 <script>
@@ -36,7 +72,18 @@
           title: "뷰 공식 사이트",
           label: "vuejs.org",
         },
+        isButtonDisabled:true,
+        ok:true,
+        number: 1000,
+        message: "지금까지 템플릿의 단순한 속성만 있었습니다. 그러나 Vue는 실제로 모든 데이터가 내에서 JavaScript 강화의 모든 기능을 지원합니다.",
+        id: 'genie',
+
+        seen: true,
+        awesome: true,
+        type: '9',
+
       }
+
     },
     computed:{
 
@@ -47,10 +94,13 @@
     mounted(){
 
     },
-    methods:{
-
+    methods: {
+    calculateDate() {
+      const timeStamp = Date.now();
+      const formattedString = Date.formatDate(timeStamp, "YYYY-MM-DD HH:mm");
+      return formattedString;
+		  },
     },
-
     title: "Vue Basic",
   }
 </script>
